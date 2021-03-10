05f9e30b1391b270c7e47508a1ea95ad53372a0b
Robert Ni <robertni@outlook.fr>
2021-03-10 12:09:47 -0500

    changed gui to a grid instead of list

--- app/src/main/java/edu/temple/langexchange/FlashcardActivity.java
+++ app/src/main/java/edu/temple/langexchange/FlashcardActivity.java
@@ -4,7 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ListView;
+import android.widget.Button;
+import android.widget.GridView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,7 +15,10 @@ import java.util.ArrayList;
 public class FlashcardActivity extends AppCompatActivity {
 
     TextView text;
-    ListView list;
+    GridView gridView;
+
+    Button makeFlashcardBtn;
+    Button makeQuizBtn;
 
     ArrayList<String> flashcards;
     ArrayList<String> translation;
@@ -25,8 +29,11 @@ public class FlashcardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_flashcard);
         getSupportActionBar().setTitle(R.string.flashcard_title);
 
-        text = findViewById(R.id.textView);
-        list = findViewById(R.id.flashcardList);
+        text = findViewById(R.id.flashcardText);
+        gridView = findViewById(R.id.flashcardGrid);
+
+        makeFlashcardBtn = findViewById(R.id.makeFlashcardBtn);
+        makeQuizBtn = findViewById(R.id.makeQuizBtn);
 
         text.setText(R.string.flashcard_instructions);
 
@@ -45,22 +52,31 @@ public class FlashcardActivity extends AppCompatActivity {
         translation.add("Translation 5");
 
         FlashcardAdapter adapter = new FlashcardAdapter(this, flashcards);
-        list.setAdapter(adapter);
+        gridView.setAdapter(adapter);
 
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                displayFlashcard(position);
+                Intent intent = new Intent(FlashcardActivity.this, DisplayFlashcard.class);
+                intent.putExtra("original", flashcards.get(position));
+                intent.putExtra("translation", translation.get(position));
+                startActivity(intent);
             }
         });
-    }
 
-    private void displayFlashcard(int position) {
-        Intent intent = new Intent(FlashcardActivity.this, DisplayFlashcard.class);
 
-        intent.putExtra("original", flashcards.get(position));
-        intent.putExtra("translation", translation.get(position));
+        makeFlashcardBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        startActivity(intent);
+            }
+        });
+
+        makeQuizBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
