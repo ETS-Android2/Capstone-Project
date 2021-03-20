e010ba2db0f7050c3561bd457a0c6a02eae91b2b
Duy Nguyen <tug81008@temple.edu>
2021-03-19 20:54:40 -0400

    finish setting up ChatSystem, tested with 2 users chatting back and forth

--- app/src/main/java/edu/temple/langexchange/MainActivity.java
+++ app/src/main/java/edu/temple/langexchange/MainActivity.java
@@ -12,7 +12,7 @@ import edu.temple.langexchange.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button, buttonQuiz, buttonMakeQuiz;
+    Button button, buttonQuiz, buttonStartChat;
   
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,19 +22,20 @@ public class MainActivity extends AppCompatActivity {
 
         button = findViewById(R.id.button);
         buttonQuiz = findViewById(R.id.button2);
+        buttonStartChat = findViewById(R.id.button3);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                Intent intent = new Intent(MainActivity.this, QuizActivity.class);
 
                 startActivity(intent);
             }
         });
 
-        buttonQuiz.setOnClickListener(new View.OnClickListener() {
+        buttonStartChat.setOnClickListener(new View.OnClickListener() {
           public void onClick(View v) {
-              Intent intent = new Intent(MainActivity.this, QuizActivity.class);
+              Intent intent = new Intent(MainActivity.this, ChatSystem.class);
               startActivity(intent);
           }
         });
