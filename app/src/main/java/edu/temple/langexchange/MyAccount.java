30c7dd6693e3426241566cd0ee29fc1f6f57c9fe
Robert Ni <robertni@outlook.fr>
2021-04-09 10:57:10 -0400

    modified class to allow saving username

--- app/src/main/java/edu/temple/langexchange/MyAccount.java
+++ app/src/main/java/edu/temple/langexchange/MyAccount.java
@@ -5,6 +5,7 @@ import android.app.Application;
 public class MyAccount extends Application {
 
     private static int userId;
+    private static String username;
 
     public int getUserId() {
         return userId;
@@ -12,6 +13,14 @@ public class MyAccount extends Application {
 
     public void setUserId(int userId) {
         this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
 }
