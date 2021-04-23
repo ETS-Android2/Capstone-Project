125f3a5276b36135f2406f01a3c5eb417eb319b8
Baloo1010 <tug81008@temple.edu>
2021-04-22 21:31:53 -0400

    fixed the bug where the user kill the app by going to recent apps, it doesn't update the usersno. Therefore, empty rooms are never deleted

--- app/src/main/java/edu/temple/langexchange/ChatSystem.java
+++ app/src/main/java/edu/temple/langexchange/ChatSystem.java
@@ -488,6 +488,13 @@ public class ChatSystem extends AppCompatActivity implements RoomListener {
             ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.RECORD_AUDIO},RecordAudioRequestCode);
         }
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+
     @Override
     protected void onDestroy() {
         long[] updatedUser = {0};
